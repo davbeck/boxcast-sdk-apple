9fa5c39eb14e369fbbc21f0858d6bb8135a07f25
Camden Fullmer <camdenfullmer@gmail.com>
2020-06-06 15:56:46 -0400

    Bump version.

--- Tests/InfoPlistTests.swift
+++ Tests/InfoPlistTests.swift
@@ -28,7 +28,7 @@ class InfoPlistTests: XCTestCase {
     }
     
     func testBoxCastSDKVersion() {
-        XCTAssertEqual(InfoPlist.boxCastSDKVersion, "0.5.0")
+        XCTAssertEqual(InfoPlist.boxCastSDKVersion, "0.5.1")
     }
     
 }
