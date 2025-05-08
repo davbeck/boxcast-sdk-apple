cbf44928a77c03308771f4bffcf1d74ce5223660
David Beck <code@davidbeck.co>
2025-05-08 07:33:19 -0700

    Fix swift package tests

--- Tests/MockedClientTestCase.swift
+++ Tests/MockedClientTestCase.swift
@@ -10,36 +10,41 @@ import XCTest
 @testable import BoxCast
 
 class MockedClientTestCase: XCTestCase {
-    
-    var client: BoxCastClient!
-    
-    override func setUp() {
-        super.setUp()
-        
-        // Set up mocking of the responses.
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses?.insert(MockedURLProtocol.self, at: 0)
-        client = BoxCastClient(scope: PublicScope(), configuration: configuration)
-    }
-    
-    func fixtureData(for name: String) -> Data? {
-        let bundle = Bundle(for: MockedClientTestCase.self)
-        
-        guard let resourceURL = bundle.resourceURL else {
-            return nil
-        }
-        
-        let fileManager = FileManager.default
-        let fileURL = resourceURL.appendingPathComponent("\(name).json")
-        guard fileManager.fileExists(atPath: fileURL.path) else {
-            return nil
-        }
-        
-        do {
-            let data = try Data(contentsOf: fileURL)
-            return data
-        } catch {
-            return nil
-        }
-    }
+	var client: BoxCastClient!
+
+	override func setUp() {
+		super.setUp()
+
+		// Set up mocking of the responses.
+		let configuration = URLSessionConfiguration.default
+		configuration.protocolClasses?.insert(MockedURLProtocol.self, at: 0)
+		client = BoxCastClient(scope: PublicScope(), configuration: configuration)
+	}
+
+	func fixtureData(for name: String) -> Data? {
+		#if SWIFT_PACKAGE
+			let bundle = Bundle.module
+			guard let resourceURL = bundle.resourceURL?.appending(component: "Fixtures") else {
+				return nil
+			}
+		#else
+			let bundle = Bundle(for: MockedClientTestCase.self)
+			guard let resourceURL = bundle.resourceURL else {
+				return nil
+			}
+		#endif
+
+		let fileManager = FileManager.default
+		let fileURL = resourceURL.appendingPathComponent("\(name).json")
+		guard fileManager.fileExists(atPath: fileURL.path) else {
+			return nil
+		}
+
+		do {
+			let data = try Data(contentsOf: fileURL)
+			return data
+		} catch {
+			return nil
+		}
+	}
 }
